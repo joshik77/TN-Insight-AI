437914178d2bc8f69ff0ac1b0d48dd1153a85647
joshik77 <joshikr658@gmail.com>
2026-08-30 19:25:27 +0530

    Fix OCR syntax error

--- backend/pdf_utils.py
+++ backend/pdf_utils.py
@@ -424,9 +424,7 @@ def ocr_single_page(page):
                         )
                     )
 
-                except Exception as (
-                    english_error
-                ):
+                               except Exception as english_error:
 
                     print(
                         "English OCR "
